9b9198123cd2d7922a8b631d19f64db29cec4bc3
Bruno Netto <brunonettomac@outlook.com>
2026-05-08 23:14:29 -0300

    Add a function and route to list the user's own data.

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   get "up" => "rails/health#show", as: :rails_health_check
   post "/login", to: "authentication#login"  
-  resources :users, only: [:index, :show, :create, :update, :destroy]
+  resources :users, only: [:index, :show, :create, :update, :destroy] do
+    get :me, on: :collection
+  end
   resources :body_measurements, only: [:index, :show, :create, :update, :destroy]
 end
